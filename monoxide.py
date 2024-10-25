e9b8f2c6e1c2dd50c84b833c9dda7631b3a4a4a3
jake <jakewdr@proton.me>
2024-10-25 13:49:07 +0100

    Some changes
    - Added pillow to pipfile
    - Started boilerplating main project loop
    - Added dummy image
    - Fixed taskfile

--- monoxide.py
+++ monoxide.py
@@ -1,8 +1,32 @@
-from glaze_cloak import Glaze
+# from glaze_cloak import Glaze
 from PIL import Image
+from multiprocessing import Pool
+import os
+import numpy as np
+
 
 def main() -> None:
+    imageNames = np.array(
+        list(os.listdir("src/images"))
+    )  # Gets image paths and adds appends them to a numpy array
+    pool = Pool()
+    pool.map(processImage, imageNames)
+
+
+def processImage(imageName: str) -> None:
+    imageContents = Image.open(f"src/images/{imageName}").convert("RGB")
+    imageContents.show()
+
+
+def glazeImage() -> None:
     pass
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    # projectRootPath = os.getcwd()
+
+
+def randomiseMetaData() -> None:
+    pass
+
 
 if __name__ == "__main__":
     main()
